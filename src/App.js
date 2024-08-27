726781d55b75b18f58dd45ef18fc56cecafbde1b
ANISH MAJUMDAR <anishmajumdar@ANISHs-Mackbook.local>
2024-08-27 21:22:18 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Switch, Route, Link, Routes } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/"  element={<Homepage />} />
+                <Route path="/cryptocurrencies"  element={<Cryptocurrencies />} />
+                <Route path="/crypto/:coinId"  element={<CryptoDetails />} />
+                <Route path="/news"  element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse © 2022 | All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/">Cryptocurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App
